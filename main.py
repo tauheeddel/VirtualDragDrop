0449d03eda295f6836214ca7e414199523d5daf5
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-10-26 20:36:36 -0500

    Added Settings Image

--- main.py
+++ main.py
@@ -1,34 +1,67 @@
 import cv2
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
+import cvzone
 
 cap = cv2.VideoCapture(0)
 cap.set(3,1280)
 cap.set(4,720)
 detector = HandDetector(detectionCon=0.8, maxHands=2)
+colorR=(255,0,255)
+cx, cy, w,h = 100,100,200,200
 
+
+class DragRect( ):
+    def __init__(self, posCenter, size=[200,200]):
+        self.posCenter = posCenter
+        self.size = size
+
+    def update(self, cursor):
+        cx,cy=self.posCenter
+        w,h=self.size
+
+        # If the index finger tip is in the rectangle region
+        if cx - w // 2 < cursor[0] < cx + w // 2 and cy - h // 2 < cursor[1] < cy + h // 2:
+            colorR = 0, 255, 0
+            self.posCenter = cursor
+rectList =[]
+for x in range(5):
+    rectList.append(DragRect([x*250+150,150]))
 
 while True:
     success, img = cap.read()
-    #img = cv2.flip(img, 1)
+    img = cv2.flip(img, 1)
     img =detector.findHands(img)
+    lmList, _ = detector.findPosition(img)
+
+    if lmList:
+        l,_,_=detector.findDistance(8,12, img, draw=False)
+        print(l)
+        if l<80:
+            cursor = lmList[8] # index finger tip landmark
+            for rect in rectList:
+                rect.update(cursor)
+    ## Draw Solid
+    # for rect in rectList:
+    #     cx, cy = rect.posCenter
+    #     w, h = rect.size
+    #     cv2.rectangle(img, (cx-w//2,cy-h//2), (cx+w//2,cy+h//2), colorR, cv2.FILLED)
+    #     cvzone.cornerRect(img,(cx-w//2,cy-h//2, w,h),20, rt=0)
+
+    #Draw Transparency
+    imgNew = np.zeros_like(img, np.uint8)
+    for rect in rectList:
+        cx, cy = rect.posCenter
+        w, h = rect.size
+        cv2.rectangle(imgNew, (cx-w//2,cy-h//2), (cx+w//2,cy+h//2), colorR, cv2.FILLED)
+        cvzone.cornerRect(imgNew,(cx-w//2,cy-h//2, w,h),20, rt=0)
+
+    out = img.copy()
+    alpha = 0.5
+    mask = imgNew.astype(bool)
+    out[mask]= cv2.addWeighted(img, alpha, imgNew, 1-alpha, 0)[mask]
 
 
-    cv2.rectangle(img, (100,100), (300,300), (255,0,255), cv2.FILLED)
-
-    # if hands:
-    #     #Hand 1
-    #     hand1 = hands[0]
-    #     lmList1 = hand1["lmList"] # List of 21 landmarks points
-    #     bbox1 = hand1["bbox"] # bouunding box info x,y, w, h
-    #     centerPoint1 = hand1["center"] #center of the hand cx, cy
-    #     handType1 = hand1["type"]
-    #     print(len(lmList1), lmList1)
-    #     print(bbox1)
-    #     print(centerPoint1)
-    #     print(handType1)
-
-
-
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", out)
     cv2.waitKey(1)
 
